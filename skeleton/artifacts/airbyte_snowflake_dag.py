1b211e0a4f8a586599022961ee0737e6da66b26d
Mihailo Vlajkovic <mihailo.vlajkovic@agilelab.it>
2022-11-08 14:06:16 +0000

    Change connection name from mysql to snowflake

--- skeleton/artifacts/airbyte_snowflake_dag.py
+++ skeleton/artifacts/airbyte_snowflake_dag.py
@@ -53,7 +53,7 @@ with DAG(
     snowflake_select = SnowflakeOperator(
         task_id="snowflake_select",
         sql="${{ values.destinationSql }}".split("/")[-1],
-        snowflake_conn_id="mysql",
+        snowflake_conn_id="snowflake",
         schema='public',
     )
 
