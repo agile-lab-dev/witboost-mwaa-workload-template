deaa28ec53b39931069fa95f1cf5470500a96e8e
Sahith Yerram <sahith.yerram@agilelab.it>
2022-11-17 14:21:56 +0000

    [#5] Resolve "Remove SQL part"
    # New features and improvements
    
    * Removed snowflake.sql file from artifacts folder since we are creating a new Snowflake SQL component for this job.
    * Removed the references of snowflake.sql file from all the files within the MWAA to reflect these changes.
    
    # Related issue
    
    Closes #5

--- skeleton/artifacts/snowflake.sql
+++
@@ -1,1 +0,0 @@
-insert into output_table(date, location_key, new_confirmed, new_deceased, new_recovered, new_tested, cumulative_confirmed, cumulative_deceased, cumulative_recovered, cumulative_tested) select date, location_key, new_confirmed, new_deceased, new_recovered, new_tested, cumulative_confirmed, cumulative_deceased, cumulative_recovered, cumulative_tested from google_covid_dataset;
