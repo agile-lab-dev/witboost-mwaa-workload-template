4a0d8e0368086b84906a51e245d52c562177ee82
Sahith Yerram <sahith.yerram@agilelab.it>
2023-09-28 18:28:42 +0000

    WIT-140 - Fix
    # Bug fixes
    
    Remove redundant information from the MWAA DAG artifact file.
    
    # Definition of Done
    
    ## All developments
    - [ ] Feature was implemented as per the requirements
    - [ ] If some code parts are complex they must be commented with code documentation
    - [ ] If feature affects the corresponding Governance Policy, then the policy has been updated and a new version of the policy set up (before the template is QA tested)
    - [ ] QA tests are successful (please comment with a link to the created component & tested DP)
        - Template must be tested by:
            - Creating a component with it in QA env
            - Committing the DP
            - Launching a test on the DP in the Control Panel
            - Deploying the DP
                - Mocked components are acceptable: eg for a template of a Spark workload we do not need to actually implement business logic. We don't need to actually develop on the component for every single feature.
    - [ ] Documentations have been updated
        - Both the documentation of the template, and the documentation of the component inside the skeleton, must be updated (if one, the other or both of course depends on the kind of work we're doing)
        - Documentation has been updated with explanation of the new feature if it is user-facing (eg component now has additional setting) or it impacts him in some other way (eg optional field that becomes mandatory)
        - If it is a breaking change we have documented them as such in the MR description in a "Breaking Changes" section
    
    ## Wizard development
    - [ ] The wizard design has been implemented according to the provided wireframes, mockups, or design specifications
    - [ ] The UI elements are visually consistent, aesthetically pleasing, and aligned with the brand guidelines
    - [ ] Dynamic elements, such as dropdowns, sliders, and modals, function as expected and enhance the user experience. Also edge conditions have been tested
    - [ ]  Labels everywhere are meaningful to the user. All labels have been reviewed
    - [ ] Embedded images use sensible hosting strategy
    - [ ] The wizard steps are clear to the users
    
    ## Skeleton/component template development
    - [ ] If feature affects the skeleton of a component that contains a codebase (eg introducing new feature in the codebase), unit tests for that feature are added to the codebase.
    
    # Related issue
    
    Closes WIT-140

--- skeleton/artifacts/airbyte_snowflake_dag.py
+++ skeleton/artifacts/airbyte_snowflake_dag.py
@@ -1,15 +1,10 @@
-from airflow import DAG, settings, secrets
-from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
-from airflow.providers.amazon.aws.hooks.base_aws import AwsBaseHook
-from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
-from airflow.providers.airbyte.operators.airbyte import AirbyteTriggerSyncOperator
 from datetime import timedelta
 import os
-import requests
-import json
 
-schedule_cron = "${{ values.scheduleCron }}" # The custom cron expression you want to use
+dag_name = os.path.basename(__file__).replace(".py", "")
+
+schedule_cron = "${{ values.scheduleCron }}"  # The custom cron expression you want to use
 component_identifier = "${{ values.identifier }}"
 
 default_args = {
@@ -18,16 +13,16 @@ default_args = {
     'depends_on_past': False
 }
 
-### Add your custom functions here
+# Add your custom functions here
 
-### 'os.path.basename(__file__).replace(".py", "")' uses the file name secrets-manager.py for a DAG ID of secrets-manager
+# 'os.path.basename(__file__).replace(".py", "")' uses the file name secrets-manager.py for a DAG ID of secrets-manager
 with DAG(
-        dag_id=os.path.basename(__file__).replace(".py", ""),
+        dag_id=dag_name,
         default_args=default_args,
         dagrun_timeout=timedelta(hours=2),
         start_date=days_ago(1),
-        schedule_interval=schedule_cron, 
+        schedule_interval=schedule_cron,
         is_paused_upon_creation=False
 ) as dag:
-    ### Add your dag Operators here
+    # Add your dag Operators here
     ...
